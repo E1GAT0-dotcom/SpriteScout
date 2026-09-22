411f30d33f851570c4dae2d37c3bc98357fe8c8f
E1GAT0-dotcom <1111kechairs@gmail.com>
2026-09-21 20:24:48 -0700

    Version 2.8

--- spritescout.py
+++ spritescout.py
@@ -49,7 +49,7 @@ try:
 except ImportError:
     msvcrt = None
 
-VERSION = "2.7"
+VERSION = "2.8"
 RELEASES = "https://github.com/E1GAT0-dotcom/SpriteScout/releases"
 LATEST_RELEASE = "https://api.github.com/repos/E1GAT0-dotcom/SpriteScout/releases/latest"
 FROZEN = getattr(sys, "frozen", False)  # running as SpriteScout.exe
